6287a98f44e802433865eee2ad255333dc7fa4ba
Matthew Finch <finchrmatthew@gmail.com>
2022-01-30 01:16:06 -0500

    added politician scraper

--- crawl.py
+++ crawl.py
@@ -1,18 +1,40 @@
+import argparse
 from crawlers.baudrillard import BaudrillardCrawler
+from crawlers.politician import PoliticianCrawler
 
-def scrapeBaudrillard():
 
-    crawler = BaudrillardCrawler(verbose=True)
+class Scrapers():
 
-    texts = [
-        'simulations',
-        'simulacra-and-simulations'
-    ]
+    def scrapeBaudrillard(self):
 
-    for title in texts:
-        crawler.crawl(title=title)
+        crawler = BaudrillardCrawler(verbose=True)
+
+        texts = [
+            'simulations',
+            'simulacra-and-simulations'
+        ]
+
+        [ crawler.craw(title=title) for title in texts ]
+
+
+    def scrapePoliticians(self) -> None:
+
+        crawler = PoliticianCrawler(verbose=True)
+        crawler.fetchSpeeches()
 
 
 if __name__ == '__main__':
 
-    scrapeBaudrillard()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--i', '--identity', type=str, help='name of author')
+
+    args = parser.parse_args()
+
+    if args.i == 'baudrillard':
+        Scrapers().scrapeBaudrillard()
+
+    elif args.i == 'politicians':
+        Scrapers().scrapePoliticians()
+
+    else:
+        raise Exception('please choose between: baudrillard or politicians')
--- crawlers/politician.py
+++ crawlers/politician.py
@@ -1,3 +1,5 @@
+import os
+import re
 import requests
 from bs4 import BeautifulSoup
 
@@ -25,10 +27,47 @@ class PoliticianCrawler(object):
 
     def saveSpeeches(self, speech_links: str) -> None:
 
-        pass
+        for link in speech_links:
+
+            url: str = f"{Paths.root}{link}"
+            title: str = url.split('/')[-1]
+
+            res: object = requests.get(url)
+            page: object = BeautifulSoup(res.content, 'html.parser')
+
+            text: list = [ i.get_text() for i in page.find_all('p') ]
+
+            pres_name: str = '-'.join(''.join(text[1:2]).replace('.', '').split(' ')).lower()
+            dos: str = '-'.join(''.join(text[2:3]).replace('.', '').split(' ')).lower()
+
+            if self.verbose: LOG.info(f'Speech on Date: {dos} given by {pres_name}')
+
+            try:
+                os.mkdir(f'{PoliticianCrawler.textRoot}{pres_name}')
+                if self.verbose: LOG.info(f'Directory Made: {pres_name}')
+
+            except FileExistsError:
+                _file: str = f'{PoliticianCrawler.textRoot}{pres_name}/{title}.txt'
+                with open(_file, 'w') as f:
+                    for t in text[3:-2]:
+                        f.write(f'{t}\n')
 
     def fetchSpeeches(self) -> None:
 
-        pass
+        regex: str = '/the-presidency/presidential-speeches/'
+        speech_links: list = []
 
-    
+        for page in range(0, Paths.pages):
+
+            if self.verbose: LOG.info(f'Page {page} Gathered.....')
+
+            speeches: str = f'{Paths.speeches_page}{Paths.page_url}{page}'
+
+            res: object = requests.get(speeches)
+            page: object = BeautifulSoup(res.content, 'html.parser')
+
+            links: list  = [ link['href'] for link in page.find_all('a', href=True) if re.match(regex, link['href']) ]
+
+            speech_links.extend(links)
+
+        self.saveSpeeches(speech_links=speech_links)
